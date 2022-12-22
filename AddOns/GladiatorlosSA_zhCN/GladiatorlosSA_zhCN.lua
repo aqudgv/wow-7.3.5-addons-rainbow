4ea8f305e60d41f18a5ac61ca98f4821a2357b35
aiwei <aiwei@jd.com>
2022-12-22 18:34:06 +0800

    init

--- AddOns/GladiatorlosSA_zhCN/GladiatorlosSA_zhCN.lua
+++
@@ -1,4 +0,0 @@
-
-local GSA = GladiatorlosSA
-rawset (GSA.GSA_LANGUAGE, "GladiatorlosSA_zhCN\\Voice_zhCN", "國語 (女聲)")
-rawset (GSA.GSA_LOCALEPATH, "zhTW", "GladiatorlosSA_zhCN\\Voice_zhCN")
--- AddOns/Roguebox/README.md
+++
@@ -1,2 +0,0 @@
-# Roguebox
-A World of Warcraft addon for combat rogues that tracks Roll the Bones, Crimson Vial and other effects.
